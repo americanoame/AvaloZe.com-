af8edd05e76b822553e62f052ac0a51aeed52ed8
Americano.Dev <andrenations@gmail.com>
2023-05-01 01:05:29 -0400

    implemented backend server

---
+++ backend/src/types/Products.ts
@@ -0,0 +1,12 @@
+export type Product = {
+    name: string
+    slug: string
+    image: string
+    category: string
+    brand: string
+    countInStock: number
+    description: string
+    price: number
+    rating: number
+    numReviews: number
+}
