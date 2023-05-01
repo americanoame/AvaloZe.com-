a47b4866a9271972d78b9d0b0074b957e216fb8c
Americano.Dev <andrenations@gmail.com>
2023-04-30 22:22:36 -0400

    addeing list products

---
+++ avaloze/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css';
+import { sampleProducts } from './data';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Avaloze</header>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img src={product.image} alt={product.name} className="product-image" />
+              <h2>{product.name}</h2>
+              <p>{product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <footer>All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
