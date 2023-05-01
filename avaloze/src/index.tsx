77a8bc6cfb59624f608a223a55517b56f296045a
Americano.Dev <andrenations@gmail.com>
2023-05-01 00:09:43 -0400

    implemented page routing

--- avaloze/src/index.tsx
+++ avaloze/src/index.tsx
@@ -12,11 +12,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} element={<HomePage />} /> 
+      <Route path='product/:slug' element={<ProductPage />} />
       {/* <Route path="dashboard" element={<Dashboard />} /> */}
       {/* ... etc. */}
     </Route>
