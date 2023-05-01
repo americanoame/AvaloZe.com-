09619d0dbac3ca858f4ac1a8d55aade444aa7c26
Americano.Dev <andrenations@gmail.com>
2023-04-30 23:01:29 -0400

    adding navbar and centered the images

--- avaloze/src/App.tsx
+++ avaloze/src/App.tsx
@@ -1,22 +1,43 @@
-import './App.css';
+import { Container, Navbar, Nav, Row, Col } from 'react-bootstrap'
 import { sampleProducts } from './data';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header>Avaloze</header>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg='dark' variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand>Avaloze</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href='/cart' className='mav-link'>Cart</a>
+            <a href='/Signin' className='mav-link'>Sign in</a>
+          </Nav>
+        </Navbar>
+
+      </header>
       <main>
-        <ul>
+        <Container className='mt-3'>
+        <Row>
           {sampleProducts.map((product) => (
-            <li key={product.slug}>
+            <Col key={product.slug}>
               <img src={product.image} alt={product.name} className="product-image" />
               <h2>{product.name}</h2>
               <p>{product.price}</p>
-            </li>
+            </Col>
           ))}
-        </ul>
+        </Row>
+        </Container>
       </main>
-      <footer>All right reserved</footer>
+
+      <footer>
+        <div className='text-center'>
+          All right reserved
+        </div>
+      </footer>
+
     </div>
   );
 }
