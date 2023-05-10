0a83fc3bb75868a3a5d38d3b137e4e326242b54d
Americano.Dev <andrenations@gmail.com>
2023-05-10 10:18:46 -0400

    adding product details

--- avaloze/src/pages/ProductPage.tsx
+++ avaloze/src/pages/ProductPage.tsx
@@ -1,4 +1,4 @@
-import { Col, Row } from 'react-bootstrap';
+import { Col, ListGroup, ListGroupItem, Card, Row } from 'react-bootstrap';
 import { Helmet } from 'react-helmet-async';
 import { useGetProductDetailsBySlugQuery } from '../hooks/productHooks';
 import { useParams } from 'react-router-dom';
@@ -6,6 +6,7 @@ import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { getError } from '../utils';
 import { ApiError } from '../types/ApiError';
+import Rating from '../components/Rating';
 
 export default function ProductPage() {
   const params = useParams();
@@ -21,12 +22,42 @@ export default function ProductPage() {
     <MessageBox variant="danger">Product Not Found</MessageBox>
   ) : (
     <div>
-      <Helmet>
-        <title>Avaloze/{product.slug}</title>
-      </Helmet>
       <Row>
         <Col md={6}>
           <img className="large" src={product.image} alt={product.name}></img>
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <Helmet>
+                <title>Avaloze.com/{product.name}</title>
+              </Helmet>
+              <h1>{product.name}</h1>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
+            </ListGroupItem>
+            <ListGroupItem>Price : ${product.price}</ListGroupItem>
+            <ListGroupItem>
+              Description:
+              <p>{product.description}</p>
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <Card.Body>
+              <ListGroup variant='flush'>
+               <ListGroupItem>
+                <Row>
+                  <Col>Price</Col>
+                  <Col>${product.price}</Col> 
+                </Row>
+                </ListGroupItem>
+              </ListGroup>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </div>
